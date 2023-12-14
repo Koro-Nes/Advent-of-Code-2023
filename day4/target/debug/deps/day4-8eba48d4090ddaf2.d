a6c5f3bd34870b09cb52e38c043a89aa099785d0
12123696 <e 12123696@student.tuwien.ac>
2023-12-14 20:16:13 +0100

    initialized day4

---
+++ day4/target/debug/deps/day4-8eba48d4090ddaf2.d
@@ -0,0 +1,5 @@
+C:\Users\Clemens\projects\rust\aoc23\day4\target\debug\deps\libday4-8eba48d4090ddaf2.rmeta: src\main.rs
+
+C:\Users\Clemens\projects\rust\aoc23\day4\target\debug\deps\day4-8eba48d4090ddaf2.d: src\main.rs
+
+src\main.rs:
